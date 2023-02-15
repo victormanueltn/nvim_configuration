bd328fc84f0c4597e6da5c3ec2360275ef783303
Victor Trejo <victormanueltn@gmail.com>
2023-02-15 21:07:53 +0100

    Add fidget plugin to show status of lsp.

--- lua/victor/packer.lua
+++ lua/victor/packer.lua
@@ -61,6 +61,8 @@ return require('packer').startup(function(use)
 
     }
 
+    use('j-hui/fidget.nvim')
+
    -- use {
    --     "folke/trouble.nvim",
    --     requires = "nvim-tree/nvim-web-devicons",
